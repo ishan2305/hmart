9b05649bea46750ef35f98875e9b428c74767362
iamishan15 <iamishankumar15@gmail.com>
2021-05-02 11:59:53 +0530

    second commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String search = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,61 +39,78 @@ class _HomeScreenState extends State<HomeScreen> {
               }),
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('product').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return ListView(
-            children: snapshot.data.docs.map((document) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blueAccent),
-                    borderRadius: BorderRadius.all(Radius.circular(
-                            5.0) //                 <--- border radius here
+      body: Column(
+        children: [
+          TextFormField(onChanged: (val) {
+            setState(() => search = val);
+          }),
+          StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('product').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return ListView(
+                shrinkWrap: true,
+                children: snapshot.data.docs.map((document) {
+                  if (!document['productName'].contains(search)) {
+                    return Container(
+                      height: 10.0,
+                      child: Text(''),
+                    );
+                  } else
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent),
+                          borderRadius: BorderRadius.all(Radius.circular(
+                                  5.0) //                 <--- border radius here
+                              ),
                         ),
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text(document['productName']),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text(
-                        document['description'],
-                        style: TextStyle(
-                          fontSize: 12,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(document['productName']),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(
+                              document['description'],
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.favorite),
+                                  onPressed: () {},
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.shopping_cart),
+                                  onPressed: () {},
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.favorite),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.shopping_cart),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
+                    );
+                }).toList(),
               );
-            }).toList(),
-          );
-        },
+            },
+          ),
+        ],
       ),
     );
   }
